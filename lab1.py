c6601cffb3fe4dc39c6e7db057ef927d33c85797
mingxun <mingxun@ualberta.ca>
2016-09-08 14:11:01 -0600

    Finished question 8

--- lab1.py
+++ lab1.py
@@ -2,5 +2,8 @@ import requests
 import os
 
 print requests.__version__
-print os.system('curl -iLX GET http://google.com')
-print os.system('curl -iX POST http://ccid-eddieantonio.rhcloud.com/mingxun')
+
+os.system('curl -iLX GET http://google.com')
+os.system('curl -iX POST http://ccid-eddieantonio.rhcloud.com/mingxun')
+
+os.system('curl https://raw.githubusercontent.com/UliceNix/CMPUT404Lab1/master/lab1.py')
