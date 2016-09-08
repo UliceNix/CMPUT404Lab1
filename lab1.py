c01502a35f5b9e32c86439700d2d43520cf312bb
mingxun <mingxun@ualberta.ca>
2016-09-08 15:25:00 -0600

    converted from lazy os way to requests module way

--- lab1.py
+++ lab1.py
@@ -1,9 +1,22 @@
 import requests
-import os
 
+# Question 2, 3, 4
 print requests.__version__
 
-os.system('curl -iLX GET http://google.com')
-os.system('curl -iX POST http://ccid-eddieantonio.rhcloud.com/mingxun')
+# Question 6
+r = requests.get('http://google.com')
+print 'Question 6: ' + str(r.status_code)
 
-os.system('curl https://raw.githubusercontent.com/UliceNix/CMPUT404Lab1/master/lab1.py')
+# Question 7
+r = requests.post('http://ccid-eddieantonio.rhcloud.com/mingxun')
+print 'Question 7: ' + str(r.status_code)
+
+# Question 8
+r = requests.get('https://raw.githubusercontent.com/UliceNix/CMPUT404Lab1/master/lab1.py')
+print 'Question 8: \n' + str(r.content)
+
+
+#import os
+#os.system('curl -iLX GET http://google.com')
+#os.system('curl -iX POST http://ccid-eddieantonio.rhcloud.com/mingxun')
+#os.system('curl https://raw.githubusercontent.com/UliceNix/CMPUT404Lab1/master/lab1.py')
